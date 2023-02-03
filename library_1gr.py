9e652c9919c52f7cabba79c719fb06edcdfd423f
it-ddt <45958272+it-ddt@users.noreply.github.com>
2023-02-03 16:25:27 +0300

    Add files via upload

---
+++ library_1gr.py
@@ -0,0 +1,132 @@
+library = [
+    {
+        "название": "Введение в Python. Том 1",
+        "автор": "Марк Лутц",
+        "год": 2022
+    },
+    {
+        "название": "Введение в Python. Том 2",
+        "автор": "Марк Лутц",
+        "год": 2022
+    },
+    {
+        
+        "название": "Искусство программирования",
+        "автор": "Дональд Кнут",
+        "год": 2019
+    },
+    {
+        "название": "Грокаем алгоритмы",
+        "автор": "Бхаргава Адитья",
+        "год": 2020    
+    }
+]
+
+
+def show_books() -> None:
+    """ выводит на экран все книги библиотеки, пронумеровав их с 1 """
+    
+    if not library:
+        print("Библиотека пуста")
+        return
+    for num, book in enumerate(library, 1):
+        print(f"номер на полке: {num}")
+        print(f"название: {book['название']}")
+        print(f"автор: {book['автор']}")
+        print(f"год: {book['год']}")
+        print("")
+
+
+def add_book() -> None:
+    """ добавляет книгу в библиотеку, в книге обязательно заполненыф 3 поля """
+    
+    title = input("Введите название книги: ")
+    if not title:
+        print("Ошибка! Нет названия.")
+        return
+
+    author = input("Введите имя автора книги: ")
+    if not author:
+        print("Ошибка! Нет автора.")
+        return
+
+    year = input("Введите год издания книги: ")
+    if year.isdigit():
+        year = int(year)
+    else:
+        print("Ошибка! Год должен быть целым числом.")
+        return
+
+    book = {
+        "название": title,
+        "автор": author,
+        "год": year
+    }
+
+    if book in library:
+        print("Ошибка! Такая книга уже есть.")
+        return
+
+    library.append(book)
+    print("Книга успешно добавлена в библиотеку!")
+
+
+def remove_book() -> None:
+    """ удаляет книгу из библиотеки по порядковму номеру ( >0 ) """
+    
+    num = input("Введите номер книги для удаления: ")
+    
+    if not num.isdigit():
+        print("Номер должен быть целым положительным числом")
+        return
+    else:
+       num = int(num)
+
+    idx = num -1 
+
+    if idx < 0:
+        print("Номер должен быть целым положительным числом")
+        return
+
+    if idx > len(library) - 1:
+        print("Нет такой книги")
+        return
+
+    print(f"Книга {library[idx]} удалена")
+    library.pop(idx)
+
+    
+def find_book_by_number() -> None:
+    """ Ищет книгу по порядковому номеру и показывает ее """
+    
+    num = input("Введите порядковый номер книги: ")
+
+    if not num.isdigit():
+        print("Номер должен быть целым положительным числом")
+        return
+    else:
+       num = int(num) 
+
+    idx = num - 1
+
+    if idx < 0:
+        print("Номер должен быть целым положительным числом")
+        return
+
+    if idx > len(library) - 1:
+        print("В библиотеке нет такой книги")
+        return
+
+    book = library[idx]
+
+    print("Книга найдена!")
+    print(f"номер на полке: {idx + 1}")
+    print(f"название: {book['название']}")
+    print(f"автор: {book['автор']}")
+    print(f"год: {book['год']}")
+    print("")
+
+    
+# тестирование
+show_books()
+remove_book()
